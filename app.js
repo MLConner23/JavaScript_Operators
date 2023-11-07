587d3c1047368b32a2cc8b686f4d4a4ceb27b5b3
Mackenzie Conner <mlc7777@outlook.com>
2023-11-06 18:15:00 -0600

    Update

--- app.js
+++ app.js
@@ -3,17 +3,43 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-console.log("EXERCISE 1:\n==========\n");
+let a = 20;
+let b = 4;
+let sum = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
 
-// YOUR CODE HERE
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
 
-// Exercise 2
-console.log("EXERCISE 2:\n==========\n");
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 
-// YOUR CODE HERE
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
 
-// Exercise 3
-console.log("EXERCISE 3:\n==========\n");
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
 
-// YOUR CODE HERE
+console.log(val == str3);
+console.log(val === str3);
+console.log(!isAwake);
+console.log("eleven" == str4 && val >= str3);
+console.log(!isAwake || isAwake);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
